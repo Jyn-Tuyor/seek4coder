31c82df62a95c3e5073f7cd9c35ad0cab7cb317a
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-11-18 19:48:52 +0800

    added background

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
 
-  <div class="flex flex-col justify-center items-center">
-    <div v-html="rendered"></div>
+  <div class="px-12 py-8 flex gap-8">
+    <label class="input">
+      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+          <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+          <circle cx="12" cy="7" r="4"></circle>
+        </g>
+      </svg>
+      <input type="text" required placeholder="Username" />
+    </label>
+    <button class="btn">Search</button>
   </div>
 
+  <main class="p-8">
+    <div class="readme-container p-2 shadow-2xl bg-black/50">
+      <div class="flex flex-col justify-center items-center">
+        <div v-html="rendered"></div>
+      </div>
+    </div>
+  </main>
 
 </template>
+
+<style scoped>
+.readme-container {
+  background-image: url('./assets/aero_glass.png');
+  background-size: cover;
+  background-position: center;
+}
+</style>
 
 <script>
 // import { Octokit } from 'octokit';
 import { Octokit } from '@octokit/rest';
 import { marked } from 'marked';
+
 
 export default {
   name: 'App',
@@ -93,7 +118,7 @@ export default {
 
   mounted() {
     this.getMe();
-  }, 
+  },
 
   computed: {
     rendered() {
