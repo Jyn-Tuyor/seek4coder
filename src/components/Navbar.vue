df2d29ad5b9dc677bf75f9a6bd8b44c972aa619e
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-11-21 21:36:16 +0800

    add home-link to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar bg-base-200 navbar-center">
     <div class="navbar-end">
-      <a class="btn btn-ghost text-xl">Seek4Coder</a>
+      <a class="btn btn-ghost text-xl" href="/">Seek4Coder</a>
     </div>
   </nav>
 </template>
